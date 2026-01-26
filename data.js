133fd292498dbadca036896bf0166cc937a83848
Stan Lee Cee <stanchat@tccons.com>
2026-01-26 11:32:24 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -18,9 +18,7 @@ window.SITE = {
     },
     // Put your real photo here: assets/profile.jpg (or .png)
     // Then update profileImage to "./assets/profile.jpg"
-     profileImage: "./assets/profile-placeholder.svg", 
-    
-    //profileImage: "./assets/stanchatman-profilepic.jpg",
+    profileImage: "./assets/profile-placeholder.svg",
     topSkills: ["Generative AI", "Product Strategy", "Agile/Scrum", "Change Management", "AI Governance"]
   },
 
